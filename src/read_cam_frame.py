dd90323bbce7f19f09de72b3ffe94fe894a9c75e
kwh44 <maistruk44@gmail.com>
2020-08-16 20:44:06 +0300

    Add read camera frames

--- src/read_cam_frame.py
+++ src/read_cam_frame.py
@@ -1,37 +1,30 @@
 import rclpy
 from rclpy.node import Node
-
+import rclpy.qos as qos
+import cv2
+import numpy as np
 from sensor_msgs.msg import Image
 
-
-class MinimalSubscriber(Node):
-
+class CameraRead(Node):
     def __init__(self):
-        super().__init__('minimal_subscriber')
-        self.subscription = self.create_subscription(
-            Image,
-            '/camera/link/camera/image',
-            self.listener_callback,
-            10)
-        self.subscription  # prevent unused variable warning
+        super().__init__('CameraRead')
+        self.__window_name = "img"
+        self._qos = qos.QosProfile()
+        self.sub = self.create_subscription(Image, '/rs_camera/rs_d435/image_raw', self.msg_callback)
 
-    def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+    def msg_callback(self, m):
+        print("received image stamp {}".format(m.header.frame_id))
+        np_img = np.reshape(m.data, (m.height, m.width, 3)).astype(np.uint8)
+        cv2.imshow(self.__window_name, np_img)
+        cv2.waitKey(1)
 
 
-def main(args=None):
-    rclpy.init(args=args)
-
-    minimal_subscriber = MinimalSubscriber()
-
-    rclpy.spin(minimal_subscriber)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_subscriber.destroy_node()
+def main():
+    rclpy.init(args=None)
+    node = CameraRead()
+    rclpy.spin(node)
+    node.destroy_node()
     rclpy.shutdown()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
