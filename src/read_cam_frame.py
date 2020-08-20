c10727da46fc8ec455ce07a0ec585fcebebf7360
kwh44 <maistruk44@gmail.com>
2020-08-20 16:06:49 +0300

    fix qos policy

--- src/read_cam_frame.py
+++ src/read_cam_frame.py
@@ -1,3 +1,4 @@
+
 import rclpy
 from rclpy.node import Node
 import rclpy.qos as qos
@@ -9,15 +10,15 @@ class CameraRead(Node):
     def __init__(self):
         super().__init__('CameraRead')
         self.__window_name = "img"
-        self._qos = qos.QosProfile()
-        self.sub = self.create_subscription(Image, '/rs_camera/rs_d435/image_raw', self.msg_callback)
+        self._qos = qos.QoSProfile(history=1, depth=1)
+        self.sub = self.create_subscription(Image, '/rs_camera/rs_d435/image_raw', self.msg_callback, qos_profile=self._qos)
 
     def msg_callback(self, m):
         print("received image stamp {}".format(m.header.frame_id))
         np_img = np.reshape(m.data, (m.height, m.width, 3)).astype(np.uint8)
         cv2.imshow(self.__window_name, np_img)
         cv2.waitKey(1)
-
+        #
 
 def main():
     rclpy.init(args=None)
