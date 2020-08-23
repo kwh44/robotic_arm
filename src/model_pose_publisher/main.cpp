8253c1bd0928a7c89e2167606a6146d6c1796819
kwh44 <maistruk44@gmail.com>
2020-08-23 22:30:53 +0300

    update gazebo plugin

--- src/model_pose_publisher/main.cpp
+++ src/model_pose_publisher/main.cpp
@@ -12,30 +12,45 @@
 #include "gazebo/transport/transport.hh"
 #include "gazebo/msgs/msgs.hh"
 
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+
 namespace gazebo {
+
     class ModelPose : public ModelPlugin {
-
         physics::ModelPtr model;
-        transport::PublisherPtr model_pose_pub_;
-        transport::NodePtr node_handle_;
-
+        rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr publisher;
+        rclcpp::node ros_node;
+        rclcpp::TimerBase::SharedPtr timer;
     public:
+
         void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/) {
             model = _parent;
-            std::bind(&ModelPose::OnUpdate, this);
-            node_handle_ = transport::NodePtr(new transport::Node());
-            node_handle_->Init("mara2");
-            model_pose_pub_ = node_handle_->Advertise<gazebo::msgs::Pose>("~/" + model->GetName() + "model_pose");
+            ros_node = rclcpp::Node("pose_ros_publisher");
+            publisher = ros_node.create_publisher<std_msgs::msg::Float64MultiArray>("/beer_model_pose", 10);
+            timer = std::chrono_literals::create_wall_timer(
+                    500ms, std::bind(&ModelPose::timer_callback, this));
 
         }
 
-        void OnUpdate() {
+        void timer_callback() {
+            std_msgs::Float64MultiArray message;
+            message.resize(3);
             auto pose = this->model->WorldPose();
-            gazebo::msgs::Pose msg;
-            gazebo::msgs::Set(&msg, pose);
-            model_pose_pub_->Publish(msg);
+            ignition::math::Vector3 v(0, 0, 0);
+            v = pose.Pos();
+            double vec[3];
+            for (size_t i = 0; i < 3; ++i) vec[i] = v[i];
+            message.data = vec;
+            publisher->publish(message);
         }
-
     };
 
     // Register this plugin with the simulator
