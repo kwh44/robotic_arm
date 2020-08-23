171496c92b6adfc9211a89329fd8ceaac045e36d
kwh44 <maistruk44@gmail.com>
2020-08-23 22:46:19 +0300

    update gazebo plugin

--- src/model_pose_publisher/main.cpp
+++ src/model_pose_publisher/main.cpp
@@ -19,13 +19,13 @@
 #include <string>
 
 #include <rclcpp/rclcpp.hpp>
-#include <std_msgs/Float64MultiArray.h>
 
 
 namespace gazebo {
 
     class ModelPose : public ModelPlugin {
         physics::ModelPtr model;
+    public:
         rclcpp::Publisher<gazebo::msgs::Pose>::SharedPtr publisher;
         rclcpp::Node ros_node;
         rclcpp::TimerBase::SharedPtr timer;
@@ -33,11 +33,14 @@ namespace gazebo {
 
         void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/) {
             model = _parent;
+            int argc = 0;
+            char **argv = NULL;
+            rclcpp::init(argc, argv);
             ros_node = rclcpp::Node("pose_ros_publisher");
             publisher = ros_node.create_publisher<gazebo::msgs::Pose>("/beer_model_pose", 10);
             timer = create_wall_timer(
                     500ms, std::bind(&ModelPose::timer_callback, this));
-
+            rclcpp::spin(ros_node);
         }
 
         void timer_callback() {
