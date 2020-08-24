38629fcb02f5dd84710343087cf7b879fbe7c396
kwh44 <maistruk44@gmail.com>
2020-08-24 12:23:32 +0300

    update gazebo plugin

--- src/model_pose_publisher/main.cpp
+++ src/model_pose_publisher/main.cpp
@@ -19,29 +19,38 @@
 #include <string>
 
 #include <rclcpp/rclcpp.hpp>
-
+/*
 
 namespace gazebo {
 
     class ModelPose : public ModelPlugin {
 
-    private:
-        physics::ModelPtr model;
-        rclcpp::Publisher<gazebo::msgs::Pose>::SharedPtr publisher;
-        rclcpp::Node ros_node;
-        rclcpp::TimerBase::SharedPtr timer;
+        private: physics::ModelPtr model;
+        private: std::unique_ptr<ros::NodeHandle> rosNode;
+        /// \brief A ROS callbackqueue that helps process messages
+        private: rclcpp::CallbackQueue rosQueue;
+        /// \brief A ROS subscriber
+        private: rclcpp::Publisher rosPub;
+        /// \brief A ROS callbackqueue that helps process messages
+        private: rclcpp::CallbackQueue rosQueue;
+        /// \brief A thread the keeps running the rosQueue
+        private: std::thread rosQueueThread;
     public:
 
-        void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/) {
+        void Load(physics::ModelPtr _parent, sdf::ElementPtr ) {
             model = _parent;
             int argc = 0;
             char **argv = NULL;
             rclcpp::init(argc, argv);
             ros_node.reset(new rclcpp::Node("pose_ros_publisher"));
-            publisher = ros_node.create_publisher<gazebo::msgs::Pose>("/beer_model_pose", 10);
-            timer = create_wall_timer(
-                    500ms, std::bind(&ModelPose::timer_callback, this));
-            rclcpp::spin(ros_node);
+            // Create a named topic, and subscribe to it.
+            rclcpp::PublishOptions po =
+                    rclcpp::publishOptions::create<gazebo::msgs::Pose>(
+                            "/" + this->model->GetName() + "/pose",
+                            1,
+                            boost::bind(&ModelPose::OnRosMsg, this, _1),
+                            ros::VoidPtr(), &this->rosQueue);
+            this->rosSub = this->rosNode->subscribe(so);
         }
 
         void timer_callback() {
@@ -50,8 +59,54 @@ namespace gazebo {
             gazebo::msgs::Set(&msg, pose);
             publisher->publish(msg);
         }
+            /// \brief Handle an incoming message from ROS
+        /// \param[in] _msg A float value that is used to set the velocity
+        /// of the Velodyne.
+        public: void OnRosMsg(const std_msgs::Float32ConstPtr &_msg)
+            {
+                this->SetVelocity(_msg->data);
+            }
+
+        /// \brief ROS helper function that processes messages
+        private: void QueueThread()
+            {
+                static const double timeout = 0.01;
+                while (this->rosNode->ok())
+                {
+                    this->rosQueue.callAvailable(ros::WallDuration(timeout));
+                }
+            }
     };
 
     // Register this plugin with the simulator
     GZ_REGISTER_MODEL_PLUGIN(ModelPose)
 }
+*/
+class ModelPose : public ModelPlugin
+{
+private: rclcpp::NodeHandle* rosnode_;
+private: rclcpp::Publisher pub_;
+private: rclcpp::PubQueue<std_msgs::String>::Ptr pub_Queue;
+private: rclcpp::PubMultiQueue pmq;
+
+public: virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+    {
+        if (!rclcpp::isInitialized())
+        {
+            int argc = 0;
+            char **argv = NULL;
+            rclcpp::init(argc, argv, "TestSpace", ros::init_options::NoSigintHandler);
+        }
+
+        this->rosnode_ = new rclcpp::NodeHandle("TestSpace");
+        this->pmq.startServiceThread();
+        this->pub_Queue = this->pmq.addPub<std_msgs::String>();
+        this->pub_ = this->rosnode_->advertise<std_msgs::String>("/beer/model_pose", 100);
+
+        std_msgs::String msg;
+        msg.data = "0.4404040404 0.2342345 0.234532245433243";
+        this->pub_Queue->push(msg, this->pub_);
+    }
+};
+
+GZ_REGISTER_MODEL_PLUGIN(VelodynePlugin);
