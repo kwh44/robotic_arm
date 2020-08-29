3af2c5dce11778124c1e3bbbd38dc8673f1b39e4
kwh44 <maistruk44@gmail.com>
2020-08-29 13:27:04 +0300

    add reset function

--- src/gym-robotic-arm.py
+++ src/gym-robotic-arm.py
@@ -1,12 +1,13 @@
 import gym
 import rclpy
 import cv2
-import math
 import numpy as np
+import rclpy.qos as qos
 from gym import spaces
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Point
-import rclpy.qos as qos
+from gazebo_msgs.srv import SpawnEntity
+from gazebo_msgs.srv import DeleteEntity
 from rclpy.qos import qos_profile_sensor_data
 from hrim_actuator_rotaryservo_msgs.msg import GoalRotaryServo
 from hrim_actuator_gripper_srvs.srv import ControlFinger
@@ -31,21 +32,27 @@ class RoboticArm(gym.Env):
             dtype=np.uint8
         )
 
-        self.target_obj1_pos = np.array([0.5, -0.5, 0.0])
-        self.target_obj2_pos = np.array([-0.5, -0.5, 0.0])
+        self.target_obj1_pos = np.array([0.5, -0.5, 0.0]) # coke_can
+        self.target_obj2_pos = np.array([-0.5, -0.5, 0.0]) # beer
 
         rclpy.init(args=None)
         self.__start_video_feed()
         self.__start_arm_control()
+        self.__init_spawn()
+        self.__init_delete()
 
     def reset(self):
         """
         reset() called at the beginning of an episode, it returns an observation
         """
-        # set joints and gripper to default position
-        # move coke and beer to start position
-        # TODO
-        pass
+        # delete coke_can and beer models
+        self.__delete()
+        # spawn them in the origin position
+        self.__spawn()
+        # set joint to default position
+        action_cmd = [0., 0., 0., 0., 45., 0., 0.8]
+        self.__publish_arm_cmds(action_cmd)
+        return self.__get_observation()
 
     def step(self, action):
         """
@@ -67,6 +74,46 @@ class RoboticArm(gym.Env):
     def close(self):
         pass
 
+    def __init_spawn(self):
+        self.entity_spawner = rclpy.create_node("entity_spawner")
+        self.spawn_client = self.entity_spawner.create_client(SpawnEntity, "/spawn_entity")
+        if not self.spawn_client.service_is_ready():
+            self.spawn_client.wait_for_service()
+
+    def __init_delete(self):
+        self.del_node = rclpy.create_node("entity_delete")
+        self.del_client = self.del_node.create_client(DeleteEntity, "/delete_entity")
+        if not self.del_client.service_is_ready():
+            self.del_client.wait_for_service()
+
+    def __spawn(self):
+        models = [["beer", "/root/robotic_arm/models/beer/model.sdf", (-0.18, 0.5, 0.)],
+                  ["coke_can", "/root/robotic_arm/models/coke_can/model.sdf", (0.24, 0.5, 0.)]]
+        for model in models:
+            request = SpawnEntity.Request()
+            request.name = model[0]
+            request.xml = open(model[1], 'r').read()
+            request.robot_namespace = "spawned_object"
+            request.initial_pose.position.x, request.initial_pose.position.y, request.initial_pose.position.z = model[2]
+            done = False
+            while not done:
+                future = self.spawn_client.call_async(request)
+                rclpy.spin_until_future_complete(self.entity_spawner, future)
+                if future.result().success:
+                    done = True
+
+    def __delete(self):
+        models = ["coke_can", "beer"]
+        for model in models:
+            request = DeleteEntity.Request()
+            request.name = model
+            done = False
+            while not done:
+                future = self.del_client.call_async(request)
+                rclpy.spin_until_future_complete(self.del_node, future)
+                if future.result().success:
+                    done = True
+
     def reward_function(self):
         reward = 0.0
         obj1_pos, obj2_pos = self.__get_object_pos()
@@ -77,11 +124,11 @@ class RoboticArm(gym.Env):
     def is_episode_over(self):
         dist = 0.1
         obj1_pos, obj2_pos = self.__get_object_pos()
-        return self.__distance(obj1_pos, self.target_obj1_pos) < dist and self.__distance(obj2_pos, self.target_obj2_pos) < dist
-
+        return self.__distance(obj1_pos, self.target_obj1_pos) < dist and self.__distance(obj2_pos,
+                                                                                          self.target_obj2_pos) < dist
 
     def __distance(self, p1, p2):
-        return np.sqrt(np.sum((p1-p2)**2, axis=0))
+        return np.sqrt(np.sum((p1 - p2) ** 2, axis=0))
 
     def __get_object_pos(self):
         rclpy.spin_once(self.object_type_1_node)
@@ -130,16 +177,16 @@ class RoboticArm(gym.Env):
             for y in range(1, 3):
                 node = rclpy.create_node("mara_arm_cmd_" + str(x) + "_" + str(y))
                 self.arm_cmd_nodes_pubs.append([node, node.create_publisher(GoalRotaryServo,
-                                                                           '/hrim_actuator_rotaryservo_00000000000'
+                                                                            '/hrim_actuator_rotaryservo_00000000000'
                                                                             + str(x) + '/goal_axis' + str(y),
-                                                                           qos_profile=qos_profile_sensor_data)])
+                                                                            qos_profile=qos_profile_sensor_data)])
         # declare joint angle messages
         self.arm_cmd_msgs = []
         for i in range(6):
             msg = GoalRotaryServo()
-            msg.position = 0.0                        # Position to rads
-            msg.velocity = 30.                        # Velocity in rads/s
-            msg.control_type = 4                      # Position and velocity control
+            msg.position = 0.0  # Position to rads
+            msg.velocity = 30.  # Velocity in rads/s
+            msg.control_type = 4  # Position and velocity control
             self.arm_cmd_msgs.append(msg)
 
         # gripper node and service client
@@ -161,9 +208,11 @@ class RoboticArm(gym.Env):
 
     def __start_entity_state(self):
         self.object_type_1_node = rclpy.create_node("mara_object_type_1_node")
-        self.sub_object_type_1 = self.object_type_1_node.create_subscription(Point, '/spawned_object/coke_can_entity_state', self.__object_type_1_callback,
-                                                             qos_profile=self._qos)
+        self.sub_object_type_1 = self.object_type_1_node.create_subscription(Point,
+                                                                             '/spawned_object/coke_can_entity_state',
+                                                                             self.__object_type_1_callback,
+                                                                             qos_profile=self._qos)
         self.object_type_2_node = rclpy.create_node("mara_object_type_2_node")
-        self.sub_object_type_2 = self.object_type_2_node.create_subscription(Point, '/spawned_object/beer_entity_state', self.__object_type_2_callback,
-                                                             qos_profile=self._qos)
-
+        self.sub_object_type_2 = self.object_type_2_node.create_subscription(Point, '/spawned_object/beer_entity_state',
+                                                                             self.__object_type_2_callback,
+                                                                             qos_profile=self._qos)
